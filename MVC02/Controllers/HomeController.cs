f464375c75fe5f755eff1a6fc1b2154eb9b360f6
saraglavin <hej@saraglavin.se>
2019-02-07 16:05:05 +0100

    Har gjort så kontakformuläret sparas i en textfil.

--- MVC02/Controllers/HomeController.cs
+++ MVC02/Controllers/HomeController.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MVC02.Models;
+using MVC02.Models.ViewModels;
+using System.IO;
+using System.Text;
+
 
 namespace MVC02.Controllers
 {
@@ -27,6 +31,20 @@ namespace MVC02.Controllers
             ViewData["Message"] = "Your contact page.";
 
             return View();
+        }
+
+        public IActionResult ContactDone(ContactVm contact)
+        {
+
+
+            string path = @"C:\Project\AcceleratedLearning\MVC\MVCProject02\MVC02\Data\Textfile\Messages.txt";
+            string newthing = $"Namn: {contact.Name}, Email: {contact.Email}, Subject: {contact.Subject}, Message: {contact.Message} \n";
+            using (StreamWriter sw = System.IO.File.AppendText(path))
+            {
+                sw.WriteLine(newthing);
+            }
+
+            return View("Contact");
         }
 
         public IActionResult Privacy()
---
+++ MVC02/Models/ViewModels/ContactVm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC02.Models.ViewModels
+{
+    public class ContactVm
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+
+
+    }
+}
