39abda1ae4cb62bda150af2f7fb6210dfae2cd92
Elin Johansson <elinmariejohansson@gmail.com>
2019-02-07 12:33:27 +0100

    no message

--- MVC02/Controllers/ProductsController.cs
+++ MVC02/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -24,7 +25,7 @@ namespace MVC02.Controllers
         // GET: Products
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Product.Include(x => x.Category).ToListAsync());
+            
         }
 
         public async Task<IActionResult> ShowByCategory(int? id) {
