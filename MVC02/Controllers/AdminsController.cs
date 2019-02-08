3e3cc58a784b06a372b1fcafab9bb189696beb05
Elin Johansson <elinmariejohansson@gmail.com>
2019-02-08 15:02:54 +0100

    no message

--- MVC02/Controllers/AdminsController.cs
+++ MVC02/Controllers/AdminsController.cs
@@ -24,39 +24,11 @@ namespace Mvc02.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var vm = new AddRoleVm();
+            vm.AllRoles = _context.Roles.Select(role => new SelectListItem() { Text = role.Name, Value = role.Id.ToString() });
+            vm.AllUsers = _context.Users.Select(user => new SelectListItem() { Text = user.Email, Value = user.Id.ToString() });
+            return View("Index", vm);
         }
-
-        public IActionResult AddRoleForUser(AddRoleVm addrole) // Tog bort AddRoleVm addrole i parentesen
-        {
-            //Vi vill kolla om användaren har rollen sedan innan
-
-
-            //var truefalse = _auth.UserExist(addrole.Email);
-
-            //var vm = new AddRoleVm();
-            addrole.AllRoles = _context.Roles.Select(role => new SelectListItem() { Text = role.Name, Value = role.Id.ToString() });
-           addrole.AllUsers = _context.Users.Select(user => new SelectListItem() { Text = user.Email, Value = user.Id.ToString() });
-            return View("Index", addrole);
-
-            //if (await truefalse)
-            //{
-            //    if (!ModelState.IsValid)
-            //    {
-            //        return View("Index");
-            //    }
-
-            //    await _auth.AddRoleToUser(addrole.Email, addrole.Role);
-            //    return View("SuccesAddRole", addrole);
-
-            //}
-
-            //else
-            //{
-            //    ModelState.AddModelError("UserDontExist", $"Användaren med email {addrole.Email} finns inte.");
-            //    return View("Index");
-            //}
-
-        }
+        
     }
 }
